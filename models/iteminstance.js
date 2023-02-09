5b1c6b311a8d671b664a8a5a36b85e767aaa7d47
afelty <afelty15@gmail.com>
2023-02-09 16:56:40 -0600

    Create schemas for all three models

---
+++ models/iteminstance.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemInstanceSchema = new Schema({
+  item: { type: Schema.Types.ObjectId, ref: "Item", required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Available", "Low Stock", "Out of Stock"],
+    default: "Out of Stock",
+  },
+});
+
+ItemInstanceSchema.virtual("url").get(function () {
+  return `/catalog/iteminstance/${this._id}`;
+});
+
+//Export module
+module.exports = mongoose.model("BookInstance", BookInstanceSchema);
